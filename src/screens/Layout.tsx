c9ac5766a1fdb4679203b99e11daa6fc4d38190d
giovanni nady <jovany.nady25@gmail.com>
2025-04-16 11:54:33 +0200

    add eslint and prettier

---
+++ src/screens/Layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Layout() {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+          <Text className='text-blue-900'>
+            Open up App.ts to start working on your app!
+          </Text>
+        </View>
+  )
+}
